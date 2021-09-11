fd8c97b4202e0785fa92e387370b5bba364d2608
SarvarKhalimov <khalimovsarvar@gmail.com>
2021-09-12 00:54:19 +0500

    Add content to Progress component

--- src/components/presentation/Progress.js
+++ src/components/presentation/Progress.js
@@ -1,20 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import Navbar from './Navbar';
 import Footer from './Footer';
 
 const Progress = () => {
-  if (sessionStorage.getItem('token') === 'undefined' || sessionStorage.getItem('token') === null) {
-    return <Redirect to='/log_in'/> 
-  } else {
-    return (
-      <div>
-        <Navbar title = "Progress" />
-        <h1>Progress page</h1>
-        <Footer />
-      </div>
-    );
-  }
+    const [results, setResults] = useState([]);
+    useEffect(() => {
+        fetchResults();
+    }, []);
+
+    const fetchResults = () => {
+        const url = 'https://dry-brushlands-93092.herokuapp.com/results';
+        const token = sessionStorage.getItem('token');
+        console.log(token);
+        fetch(url, {
+          headers: {
+            Authorization: token,
+          }
+        })
+          .then(res => res.json())
+          .then(data => setResults(data));
+    }
+
+    if (sessionStorage.getItem('token') === 'undefined' || sessionStorage.getItem('token') === null) {
+        return <Redirect to='/log_in'/> 
+    } else {
+        return (
+        <div>
+            <Navbar title = "Progress" />
+                <main>
+                    <h2>Progress page</h2>
+                    {
+                        results.map((res) => (
+                            <div>{res.overall_score}</div>
+                        ))
+                    }
+                </main>
+            <Footer />
+        </div>
+        );
+    }
 }
 
 export default Progress;
