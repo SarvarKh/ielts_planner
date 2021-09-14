6646ad2f6da419f71a140b5cf06aa225a75ac3b9
SarvarKhalimov <khalimovsarvar@gmail.com>
2021-09-14 18:21:10 +0500

    Add planned chart data to chart component

--- src/components/presentation/MyChart.js
+++ src/components/presentation/MyChart.js
@@ -1,14 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Chart } from 'react-charts'
 
 const MyChart = ({results}) => {
-    
-    // console.log('results: ', results);
-    function defChartDate(results) {
+    function defChartData(results) {
         let arr = []
         results.map((res) => {
-            console.log(res.created_at.substring(0, 10));
-            arr.push([res.id, parseFloat(res.overall_score)]);
+            arr.push([results.indexOf(res)+1, parseFloat(res.overall_score)]);
             return arr
         })
 
@@ -20,13 +17,41 @@ const MyChart = ({results}) => {
         return output;
     }
 
+    // Define Planned Chart
+    // diff_score = planned 8.0 - initial score 6.0 => 2.0
+    // diff_period = planned 2022-04-30 - initial 2021-09-30 => 210 days
+    // required_daily_progress = diff_score 2.0 / diff_period 210 days = 0.01
+    
+    function defPlannedChartData() {
+        let planned_score = 8.0;
+        let initial_score = 6.0;
+        let diff_score = planned_score - initial_score;
+
+        // let planned_date = "2022-04-30";
+        // let initial_date = "2021-09-30";
+        // let diff_date = planned_date - initial_date;
+        let diff_date = 30
+
+        let required_daily_progress = diff_score/diff_date;
+
+        let arr = [[0, initial_score]]
+        let current = initial_score;
+        for (let i = 1; i <= diff_date; i++) {
+            arr.push([i, current += required_daily_progress])
+        }
+
+        let output = {
+            label: 'Series 3',
+            data: arr
+        }
+
+        return output;
+    }
+
     const data = React.useMemo(
         () => [
-            defChartDate(results),
-            {
-                label: 'Series 2',
-                data: [[0, 3], [1, 1], [2, 5], [3, 6], [4, 4]]
-            }
+            defChartData(results),
+            defPlannedChartData()
         ]
     )
 
@@ -43,8 +68,8 @@ const MyChart = ({results}) => {
         // space of its parent element automatically
         <div
         style={{
-            width: '400px',
-            height: '300px'
+            width: '22rem',
+            height: '12rem'
         }}
         >
         {results.length > 0 && <Chart data={data} axes={axes} />}
