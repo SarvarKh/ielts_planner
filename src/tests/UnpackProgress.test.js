797a05ecc349180bb6edd42a64323a5d661d5454
SarvarKhalimov <khalimovsarvar@gmail.com>
2021-09-18 11:06:11 +0500

    Fix UnpackProgress test console errors

--- src/tests/UnpackProgress.test.js
+++ src/tests/UnpackProgress.test.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import store from '../store';
-import renderer from 'react-test-renderer';
+import renderer, { act } from 'react-test-renderer';
 import UnpackProgress from '../components/presentation/UnpackProgress';
 import { BrowserRouter } from 'react-router-dom';
 
 it('renders correctly', () => {
   const results=[]
+  let tree
+  act(() => {
+    tree = renderer.create(
+      <Provider store={store}>
+          <React.StrictMode>
+              <BrowserRouter>
+                  <UnpackProgress results={results}/>
+              </BrowserRouter>
+          </React.StrictMode>
+      </Provider>
+    ).toJSON();
+  });
 
-  const tree = renderer.create(
-    <Provider store={store}>
-        <React.StrictMode>
-            <BrowserRouter>
-                <UnpackProgress results={results}/>
-            </BrowserRouter>
-        </React.StrictMode>
-    </Provider>
-  ).toJSON();
   expect(tree).toMatchSnapshot();
 });
